005eba9b6880c996e1c3794a3a4b4c26958a4b21
Jon Zumbrun <doccer@JZ-MacBook-Air.local>
2017-08-21 14:55:20 -0500

    fix controller

--- server/routes/engine/controllers/engine_controller.js
+++ server/routes/engine/controllers/engine_controller.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs'),
     config = require('../../../config'),
-    engine = require('../lib/engine'),
+    Engine = require('../lib/engine'),
     format = require('../lib/format')
 
 module.exports = (server) => {
@@ -12,9 +12,13 @@ module.exports = (server) => {
      */
     server.post('/engine', (req, res) => {
 
+        var engine = new Engine()
+        engine.setPlayer(req.body.user_id, req.body.user_name)
+        engine.setCommand(req.body.text)
+
         // Slack
         //if(req.body.token == config.chats.slack.token){
-            return res.json(format.slack(engine.run(req.body.text, req.body.user_id)))
+            return res.json(format.slack(engine.run()))
         //}
 
         return res.send('Chat validation failed.')
